371e590697f12c1930ffd0ee4514953f6191f8e0
Daiane da Rosa <daianedarosadev@gmail.com>
2022-02-22 21:16:13 -0300

    ✨ Adicionei a pasta "Fundamentos" e "Ínicio" juntamente com 7 Arquivos.

---
+++ FUNDAMENTOS/ínicio/bloco.js
@@ -0,0 +1,12 @@
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+}
+/*
+Blocos no j.s serve para criar/agrupar até centenas de sentenças se eu presisar ou quiser.
+ */
---
+++ FUNDAMENTOS/ínicio/sentenca_01.js
@@ -0,0 +1,10 @@
+console.log("Passo #01");
+console.log("Passo #02");
+console.log("Passo #03");
+console.log("Sentença de códigos");
+console.log("Bom dia!!")
+console.log("Boa tarde!!");
+console.log("Boa noite!!");
+/*
+Sentença corresponde a um passo do algoritmo ou a uma declaração. Se o código fonte descreve um algoritmo, então sentenças correspondem a passos do algoritmo, se não forem declarações.
+*/
